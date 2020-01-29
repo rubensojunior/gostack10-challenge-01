1384af9477bfb3575a1363f4befcdb24297c42e1
rubensojunior <jroliveirati@gmail.com>
2020-01-29 00:34:50 -0300

    post projects route created

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const express = require('express')
 
 const server = express() 
 
+server.use(express.json())
+
 const projects = []
 
 server.get('/projects', (req, res) => {
     return res.json(projects)
+})
+
+server.post('/projects', (req,res) => {
+    console.log(req.body)
+    const { id } = req.body
+    const { title } = req.body
+
+    const project = {
+        id,
+        title,
+        tasks: []
+    }
+
+    projects.push(project)
+
+    return res.json(projects)
 })
 
 server.listen(3000)
