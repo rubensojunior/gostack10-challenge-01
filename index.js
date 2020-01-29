5b0c730985b0b07c4451aff2f0dde50a87989ab2
rubensojunior <jroliveirati@gmail.com>
2020-01-29 00:50:40 -0300

    update projects route created

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ server.get('/projects', (req, res) => {
     return res.json(projects)
 })
 
-server.post('/projects', (req,res) => {
-    console.log(req.body)
+server.post('/projects', (req, res) => {
     const { id } = req.body
     const { title } = req.body
 
@@ -22,6 +21,19 @@ server.post('/projects', (req,res) => {
     }
 
     projects.push(project)
+
+    return res.json(projects)
+})
+
+server.put('/projects/:id', (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+
+    projects.forEach((p, index) => {
+        if(p.id === id){
+            projects[index].title = title  
+        }
+    })
 
     return res.json(projects)
 })
